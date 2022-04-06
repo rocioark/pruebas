259074e682a0c9d9be93c3a0662e330f4ad900a5
Rocio Agudelo <rocioark@hotmail.com>
2022-04-06 12:27:56 -0500

    subida inicial

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    static int sumar(int a, int b){
+        return a+b;
+
+    }
+    public static void main( String[] args )
+    {
+        System.out.println(sumar(1,7) );
+    }
+}
---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+    @Test
+    public void testSumar(){
+        assertEquals(App.sumar(7,1), 8);
+    }
+ }
